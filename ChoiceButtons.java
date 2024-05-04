77a2b6416bc710bb21e45ac3502b056f682670bc
Ruselle Daanoy <133234541+RuselleDaanoy@users.noreply.github.com>
2024-05-04 20:53:18 +0800

    ProjectChoicesButtons

---
+++ ChoiceButtons.java
@@ -0,0 +1,40 @@
+//package here::
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class ChoiceButtons extends JButton {
+    private static final Color HOVER_COLOR = Color.decode("#38B6FF");
+    private static final Color NORMAL_COLOR = new Color(0x191919);
+    private static final Color TEXT_COLOR = Color.WHITE;
+
+    public ChoiceButtons(String text) {
+        super(text);
+        setupButton();
+        addHoverEffect();
+    }
+
+    private void setupButton() {
+        setBackground(NORMAL_COLOR);
+        setForeground(TEXT_COLOR);
+        setFont(new Font("Arial", Font.BOLD, 15));
+        setFocusable(false);
+    }
+
+    private void addHoverEffect() {
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                setBackground(HOVER_COLOR);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                setBackground(NORMAL_COLOR);
+            }
+        });
+    }
+
+}
