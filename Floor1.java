e9261d265af90edc6bc59d8083f8b0632b002757
Thoby Jim Ralleta <152825042+thobidogs@users.noreply.github.com>
2024-05-05 19:38:33 +0800

    1st and 3rd Floor Pictures to be added

---
+++ Floor1.java
@@ -0,0 +1,78 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Floor1 extends JFrame implements ActionListener{
+
+    private ChoiceButtonsWhiteBG returnButton;
+
+    public Floor1() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        setTitle("Campus Route Path Navigator");
+        setSize(new Dimension(1280, 720));
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // change path
+        ImageIcon icon = new ImageIcon("");
+        setIconImage(icon.getImage());
+
+        add(mainPanel());
+    }
+
+    private JPanel mainPanel() {
+        JPanel panel = new JPanel(null);
+        panel.setBounds(0, 0, 1280, 720);
+        panel.setBackground(Color.WHITE);
+
+        JPanel subHeading = new JPanel();
+        subHeading.setBounds(500, 50, 300, 35);
+        subHeading.setBackground(Color.WHITE);
+        panel.add(subHeading);
+
+        JLabel subHeadingLabel = new JLabel("WALK TO THE RIGHT ROOM");
+        subHeadingLabel.setBounds(420, 60, 300, 30);
+        subHeadingLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+        subHeadingLabel.setForeground(Color.decode("#38B6FF"));
+        subHeading.add(subHeadingLabel);
+
+        JPanel heading = new JPanel();
+        heading.setBounds(100, 70, 1080, 90);
+        heading.setBackground(Color.WHITE);
+        panel.add(heading);
+
+        JLabel headingLabel = new JLabel("MAIN BUILDING - FLOOR 1");
+        headingLabel.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 68)); 
+        headingLabel.setForeground(Color.decode("#191919"));
+        heading.add(headingLabel);
+
+        ImageIcon floor = new ImageIcon("");
+        JLabel floorPlan = new JLabel(floor);
+        floorPlan.setBounds(0, 60, 1280, 600);
+        panel.add(floorPlan);
+
+        returnButton = new ChoiceButtonsWhiteBG("RETURN", "FLOOR");
+        returnButton.setBounds(560, 600, 130, 40);
+        returnButton.addActionListener(this);
+        panel.add(returnButton);
+
+        return panel;
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == returnButton) {
+            dispose();
+        }
+    }
+    
+    public static void main(String[] args) {
+        Floor1 floor = new Floor1();
+        floor.setVisible(true);
+    }
+}
