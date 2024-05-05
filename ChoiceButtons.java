c48c2b7e950c11abc00654bfc1182090cc8c343a
Thoby Jim Ralleta <152825042+thobidogs@users.noreply.github.com>
2024-05-05 20:12:06 +0800

    updated to 'changing' text effect

--- ChoiceButtons.java
+++ ChoiceButtons.java
@@ -8,16 +8,22 @@ public class ChoiceButtons extends JButton {
     private static final Color HOVER_COLOR = Color.decode("#38B6FF");
     private static final Color NORMAL_COLOR = new Color(0x191919);
     private static final Color TEXT_COLOR = Color.WHITE;
+    private String textEffect;
+    private String textNormal;
 
-    public ChoiceButtons(String text) {
+    public ChoiceButtons(String text, String change) {
         super(text);
+        this.textNormal = text;
+        this.textEffect = change;
         setupButton();
         addHoverEffect();
+
     }
 
     private void setupButton() {
         setBackground(NORMAL_COLOR);
         setForeground(TEXT_COLOR);
+        setSize(130, 40);
         setFont(new Font("Arial", Font.BOLD, 15));
         setFocusable(false);
         setBorder(BorderFactory.createLineBorder(TEXT_COLOR, 1));
@@ -29,12 +35,14 @@ public class ChoiceButtons extends JButton {
             public void mouseEntered(MouseEvent e) {
                 setBackground(HOVER_COLOR);
                 setBorder(BorderFactory.createLineBorder(HOVER_COLOR, 1));
+                setText(textEffect);
             }
 
             @Override
             public void mouseExited(MouseEvent e) {
                 setBackground(NORMAL_COLOR);
                 setBorder(BorderFactory.createLineBorder(TEXT_COLOR, 1));
+                setText(textNormal);
             }
         });
     }
