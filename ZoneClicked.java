00c404cb1ff6727aada2311bc9a50df68d3f570c
Faye Camille Buri <152823567+fayecamilleburi@users.noreply.github.com>
2024-05-07 05:15:38 +0800

    Visible numbers

--- ZoneClicked.java
+++ ZoneClicked.java
@@ -6,30 +6,24 @@ import java.awt.event.*;
 
 public class ZoneClicked extends JPanel implements MouseListener {
     private Color clickedBackground = Color.decode("#38B6FF");
+    private Color originalBackground = Color.WHITE;
     private Color clickedForeground = Color.WHITE;
+    private Color originalForeground = Color.decode("#191919");
     private boolean clicked = false;
 
     public ZoneClicked() {
         this.addMouseListener(this);
 
         setOpaque(true);
-        addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent e) {
-                clicked = !clicked;
-                setBackground(clicked ? Color.decode("#38B6FF") : Color.WHITE); // Change color when clicked
-            }
-        });
+        setBackground(originalBackground); 
     }
 
     @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-    }
-    
-    @Override
     public void mouseClicked(MouseEvent e) {
-        this.setBackground(clickedBackground);
-        setChildForeground(this, clickedForeground); 
+        clicked = !clicked;
+        setBackground(clicked ? clickedBackground : originalBackground); 
+        setForeground(clicked ? clickedForeground : originalForeground); 
+        repaint(); 
     }
 
     @Override
@@ -43,16 +37,6 @@ public class ZoneClicked extends JPanel implements MouseListener {
 
     @Override
     public void mouseExited(MouseEvent e) {}
-
-    private void setChildForeground(Container container, Color color) {
-        Component[] components = container.getComponents();
-        for (Component component : components) {
-            if (component instanceof Container) {
-                setChildForeground((Container) component, color);
-            }
-            component.setForeground(color);
-        }
-    }
 
     public boolean isClicked() {
         return clicked;
